3917cd05264dca2c9474d1b0f534512c85970371
mcdag <mcdag@cin.ufpe,br>
2022-04-16 01:33:26 -0300

    Search abr added to the parents task page

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import { Switch, RouteProps, useRouteMatch, Redirect } from 'react-router-dom';
-
 import {
-  AppLayout,
   Home,
 } from '../views';
+import ParentTasks from '../views/ParentTasks';
 import Route from './Route';
 
 const AppRoutes: React.FC<RouteProps> = () => {
   const { path } = useRouteMatch();
 
   return (
-    <AppLayout>
       <Switch>
+        <Route path={`${path}/parent-tasks`} component={ParentTasks} />
+        <Redirect to={`${path}/parent-tasks`} />
         <Route path={`${path}/home`} component={Home} />
         <Redirect to={`${path}/home`} />
       </Switch>
-    </AppLayout>
   );
 };
 
---
+++ src/views/ParentTasks/index.tsx
@@ -0,0 +1,13 @@
+import SearchBar from "../../components/SearchBar";
+import TabBar from "../../components/TabBar";
+
+function ParentTasks() {
+  return (
+    <TabBar >
+      <SearchBar />
+      <div>ParentTasks</div>
+    </TabBar>
+  );
+}
+
+export default ParentTasks;
