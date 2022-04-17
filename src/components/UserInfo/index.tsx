43dd0d0ce9f208a42f6009be15b4536073c2eef8
mcdag <mcdag@cin.ufpe,br>
2022-04-16 23:58:38 -0300

    Avatar margin updated and button added

--- src/components/UserInfo/index.tsx
+++ src/components/UserInfo/index.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Divider, List } from '@mui/material';
+import { Divider, IconButton, List } from '@mui/material';
 import ExampleAvatar from '../../assets/avatar-example.svg';
 import './styles.scss';
 
@@ -8,7 +8,9 @@ function UserInfo() {
     <div className='user-info-container'>
       <List>
         <div className='user-info'>
-          <Avatar className='avatar' alt="Avatar da criança" src={ExampleAvatar} />
+          <IconButton className='avatar'>
+            <img src={ExampleAvatar} alt="Avatar da criança" />
+          </IconButton>
           <text className='name'> José Carlos </text>
         </div>
         <Divider />
