1d609aa7b63f3640989931c7dd77649272391b66
mcdag <mcdag@cin.ufpe,br>
2022-05-07 20:57:14 -0300

    feat: title added to the view choose login

--- src/views/ChooseLogin/index.tsx
+++ src/views/ChooseLogin/index.tsx
@@ -5,26 +5,31 @@ import './styles.scss';
 
 function ChooseLogin() {
   return (
-    <div className='login-parent-child-container'>
-      <div className='parent-login'>
-        <IconButton className='icon' href='login/parent'>
-          <img src={AdultIcon} alt='Ícone adulto' />
-        </IconButton>
-        <p className='icon-title'> Responsável </p>
+    <div className='choose-login-general-container'>
+      <div className='title-container'>
+        <p className='title-text'> Como deseja fazer login?</p>
       </div>
-      <div className='child-login'>
-        <IconButton className='icon' href='login/child'>
-          <img src={KidIcon} alt='Ícone criança' />
-        </IconButton>
-        <p className='icon-title'> Criança </p>
-      </div>
-      <div className='login-register'>
-        <p className='button-text'>Não tem uma conta ?</p>
-        <a href={`${window.location.origin}/auth/register`}>
-          <button className='button'>
-            Registre-se agora
-          </button>
-        </a>
+      <div className='login-parent-child-container'>
+        <div className='parent-login'>
+          <IconButton className='icon' href='login/parent'>
+            <img src={AdultIcon} alt='Ícone adulto' />
+          </IconButton>
+          <p className='icon-title'> Responsável </p>
+        </div>
+        <div className='child-login'>
+          <IconButton className='icon' href='login/child'>
+            <img src={KidIcon} alt='Ícone criança' />
+          </IconButton>
+          <p className='icon-title'> Criança </p>
+        </div>
+        <div className='login-register'>
+          <p className='button-text'>Não tem uma conta ?</p>
+          <a href={`${window.location.origin}/auth/register`}>
+            <button className='button'>
+              Registre-se agora
+            </button>
+          </a>
+        </div>
       </div>
     </div>
   );
